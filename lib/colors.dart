52ea2cfc625f2a2071df91aa52b7f6fe37db9392
G1Hu6 <r1ruru219@gmail.com>
2025-01-05 14:56:44 +0530

    creating auth text field

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const primaryColor = Color.fromRGBO(7, 233, 112, 1);
 const backgroundColor =  Color.fromARGB(255, 6, 11, 11);
 const textColor = Color.fromRGBO(241, 241, 242, 1);
@@ -12,3 +14,4 @@ const searchBarColor = Color.fromRGBO(50, 55, 57, 1);
 const dividerColor = Color.fromRGBO(37, 45, 50, 1);
 const chatBarMessage = Color.fromRGBO(30, 36, 40, 1);
 const mobileChatBoxColor = Color.fromRGBO(31, 44, 52, 1);
+final greyColor = Colors.grey.shade500;
--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/colors.dart';
 import 'package:whatsapp_ui/common/widgets/styled_button.dart';
+import 'package:whatsapp_ui/features/auth/widgets/auth_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   static const routeName = "login-screen";
@@ -11,101 +13,92 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final countryController = TextEditingController();
+  final countryCodeController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  Country pickedCountry = Country.worldWide;
+
+  void showAllCountryPicker() {
+    showCountryPicker(
+      context: context,
+      
+      showPhoneCode:
+          true, // optional. Shows phone code before the country name.
+      onSelect: (Country country) {
+        setState(() {
+          pickedCountry = country;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "Enter your phone number",
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 25,
-              ),
-            ),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: '''WhatsApp will need to verify your phone 
-                  number. Carrier changes may apply. ''',
-                style: TextStyle(fontSize: 14, color: Colors.grey.shade500),
-                children: const <TextSpan>[
-                  TextSpan(
-                      text: 'What\'s my number?',
-                      style: TextStyle(color: primaryColor)),
-                ],
-              ),
-            ),
-            const TextField(
-              cursorColor: primaryColor,
-              decoration: InputDecoration(
-                  hintText: "Choose a country",
-                  hintStyle: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 15,
-                    color: Colors.white,
-                  ),
-                  contentPadding: EdgeInsets.only(bottom: 1),
-                  enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: primaryColor)),
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: primaryColor, width: 2)),
-                  border: UnderlineInputBorder(
-                      borderSide: BorderSide(color: primaryColor)),
-                  disabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: primaryColor))),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    cursorColor: primaryColor,
-                    decoration: InputDecoration(
-                        hintText: "Choose a country",
-                        hintStyle: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                        contentPadding: EdgeInsets.only(bottom: 1),
-                        enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: primaryColor)),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: primaryColor, width: 2)),
-                        border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: primaryColor)),
-                        disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: primaryColor))),
-                  ),
+        minimum: EdgeInsets.only(top: size.height * 0.1),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Enter your phone number",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 25,
                 ),
-                Expanded(
-                  child: TextField(
-                    cursorColor: primaryColor,
-                    decoration: InputDecoration(
-                        hintText: "Choose a country",
-                        hintStyle: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 15,
-                          color: Colors.white,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  text: '''WhatsApp will need to verify your phone 
+number. Carrier changes may apply. ''',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'What\'s my number?',
+                        style: TextStyle(color: primaryColor)),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: size.width * 0.15, vertical: 20),
+                child: Column(
+                  children: [
+                    AuthTextField(
+                        onTap: showAllCountryPicker,
+                        hintText: (pickedCountry != Country.worldWide)
+                            ? pickedCountry.name
+                            : "Choose a country",
+                        hintColor: Colors.white,
+                        readOnly: true,
+                        controller: countryController),
+                    Row(
+                      children: [
+                        SizedBox(
+                            width: size.width * 0.15,
+                            child: AuthTextField(
+                              readOnly: true,
+                              hintText: "+ ${pickedCountry.phoneCode}",
+                              controller: countryCodeController,
+                            )),
+                        const SizedBox(
+                          width: 20,
                         ),
-                        contentPadding: EdgeInsets.only(bottom: 1),
-                        enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: primaryColor)),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: primaryColor, width: 2)),
-                        border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: primaryColor)),
-                        disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: primaryColor))),
-                  ),
-                )
-              ],
-            )
-          ],
+                        Expanded(
+                            child: AuthTextField(
+                                hintText: "Phone Number",
+                                controller: phoneNumberController))
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: StyledButton(text: "Next", onPressed: () {}),
